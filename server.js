33b09d4a9f678bb7ca80b7661177ed8f718cb51d
Arva-n2be <arvarahadyanputra@gmail.com>
2026-07-08 09:25:07 +0700

    Second Commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "https://taskflowio-eight.vercel.app/",
+        origin: ["http://localhost:5173", "https://taskflowio-eight.vercel.app"],
         methods: ["GET", "POST", "PUT", "DELETE"]
     }
 });
